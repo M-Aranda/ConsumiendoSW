abad0f832755f6a055dd1cf2dd14169fd0fb39c9
M-Aranda <cheloz_20@hotmail.com>
2018-10-30 07:49:27 -0300

    agregado script

---
+++ scriptBDEjemploSW.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bdEjemploSW;
+
+USE bdEjemploSW;
+
+CREATE TABLE persona(
+id INT AUTO_INCREMENT,
+nombre VARCHAR (30),
+PRIMARY KEY (id)
+);
+
+ INSERT INTO persona VALUES  (NULL, 'Marcelo');
+
